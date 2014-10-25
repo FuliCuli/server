ec89f1a7391b71d2e23eb597c13790b96dc05775
xavier <xavier@axonea.com>
2014-10-25 09:45:37 +0200

    feat(vagrant): add vagrant files

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export ANSIBLE_FORCE_COLOR=true
+export PYTHONUNBUFFERED=1
+apt-get install language-pack-UTF-8 -y
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible -y
+cd /home/ansible
+ansible-playbook -i hosts_dev webserver.yml --connection=local
